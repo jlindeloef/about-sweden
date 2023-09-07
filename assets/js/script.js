999df7d156b49ecbae1b2987af7b2b7ecd542fe7
jlindeloef <>
2023-09-07 11:42:03 +0000

    Creating the questions for the quiz and the buttons.

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Whicih is the main capitol of Sweden?",
+        answers: [
+            { text: "Milano", correct: false}, 
+            { text: "Gotenburg", correct: false }, 
+            { text: "Stockholm", correct: true},
+            { text: "Copenhagen", correct: false },
+        ]
+    },
+    {
+        question: "Which is the local currency in Sweden?",
+        answers: [
+            { text: "Euro (EUR)", correct: false},
+            { text: "Scandinavian Pound (SCP)", correct: false},
+            { text: "Swedish Crowns (SEK)", correct: true},
+            { text: "Swish (SWI)", correct: false},
+        ]
+},
+    {
+     question: "What is the name of the King of Sweden?",
+        answers: [
+            { text: "Carl XVI Gustaf", correct: true},
+            { text: "Calle", correct: false},
+            { text: "Carl VIX Gustaf", correct: true},
+            { text: "Carl IXV Gustaf", correct: false},
+        ]
+    },
+        {
+            question: "What is Kebnekaise?",
+        answers: [
+            { text: "Troll", correct: false},
+            { text: "Swedish national dish", correct: false},
+            { text: "A popular pizza in Sweden", correct: false},
+            { text: "Sweden’s highest mountain", correct: true},
+        ]
+    },
+]
